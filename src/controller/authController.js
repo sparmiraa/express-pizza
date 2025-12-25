e4a6c0ab26a51e59c431a858c6d968abb2ae9bb1
sparmira <aliceops09@gmail.com>
2025-12-25 21:38:26 +0600

    [tsk-#PIZ-5] Реализовать обновление токенов

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,5 +1,8 @@
 import authService from "../services/authService.js";
+import tokenService from "../services/tokenService.js";
 import { env } from "../config/env.config.js";
+import ApiError from "../exceptions/apiError.js";
+import userService from "../services/userService.js";
 
 class AuthController {
   async registration(req, res, next) {
@@ -30,7 +33,7 @@ class AuthController {
     try {
       const { email, password } = req.body;
 
-      const data = await authService.login(email, password)
+      const data = await authService.login(email, password);
 
       res.cookie("refreshToken", data.refreshToken, {
         httpOnly: true,
@@ -41,7 +44,43 @@ class AuthController {
         accessToken: data.accessToken,
       });
     } catch (e) {
-      next(e)
+      next(e);
+    }
+  }
+
+  async refresh(req, res, next) {
+    try {
+      const { refreshToken } = req.cookies;
+      if (!refreshToken) {
+        throw ApiError.Unauthorized();
+      }
+
+      const userData = tokenService.validateRefreshToken(refreshToken);
+      if (!userData) {
+        throw ApiError.Unauthorized();
+      }
+
+      const user = await userService.getUserById(userData.userId);
+
+      const roles = user.Roles.map((role) => role.name);
+
+      const newAccessToken = tokenService.generateAccessToken({
+        userId: user.id,
+        roles,
+      });
+
+      const newRefreshToken = tokenService.generateRefreshToken({
+        userId: user.id,
+      });
+
+      res.cookie("refreshToken", newRefreshToken, {
+        httpOnly: true,
+        maxAge: env.TIME_TO_LIVE_REFRESH_TOKEN,
+      });
+
+      return res.json({ accessToken: newAccessToken });
+    } catch (e) {
+      next(e);
     }
   }
 }
